c2c3f9a6ee98c77b8fb50ef0b5fd3fbbe0e6b9ca
Konstantin Guznov <kosguz1@gmail.com>
2020-11-05 16:36:31 +0300

    Initial commit

---
+++ src/main/java/ru/mathprog/simplex/SimplexApplication.java
@@ -0,0 +1,13 @@
+package ru.mathprog.simplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mathprog/simplex/SimplexApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mathprog.simplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
